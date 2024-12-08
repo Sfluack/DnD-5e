594b8db3792350b744bfb1f1b3fa65f32c5bd126
Sfluack <walking_in_faith@hotmail.com>
2024-12-08 11:28:21 -0600

    Updated styles, added JS function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const themeRadio = document.querySelector('input[name="theme-selector"]');
 
 themeRadio.addEventListener("change", (e) => {
+    document.getElementsByTagName("body").classList.toggle("dark-mode");
 });
