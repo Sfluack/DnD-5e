1a80e68860549e9e51d2acfbe2f089a48241e99f
Sfluack <walking_in_faith@hotmail.com>
2024-12-09 07:44:15 -0600

    Updated CSS and added notes file

---
+++ notes.js
@@ -0,0 +1,9 @@
+/*  -- Dark Mode based on browser settings
+@media (prefers-color-scheme: dark) {
+    body {
+        background-color: var(--dark-grey);
+        border-color: var(--medium-grey);
+        color: var(--light-grey);
+    }
+}
+*/
